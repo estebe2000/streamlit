902a735a194e95cf49c48b52d292f910e3a58db4
estebe2000 <46036430+estebe2000@users.noreply.github.com>
2023-04-14 18:54:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ image_upload = st.file_uploader("Choisir une image",type=['jpg','jpeg','png'])
 
 def convert_image(image):
   buf = BytesIO()
-  image.save'buf, format='PNG')
+  image.save(buf, format='PNG')
   byte_im = buf.getvalue()
   return byte_im
 
