95e4de2185f6535b59d8ec8818b426f1a65d353f
estebe2000 <46036430+estebe2000@users.noreply.github.com>
2023-04-14 18:36:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from PIL import Image
 
 
-st.title("TOUTOU")
+st.title("Remover de BG")
 
 image_upload = st.file_uploader("Choisir une image",type=['jpg','jpeg','png'])
+
+if image_upload:
+  image = Image.open(image_upload)
