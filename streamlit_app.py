bdc4d781fef6f0d4022739a5738ee0c5059f5d50
estebe2000 <46036430+estebe2000@users.noreply.github.com>
2023-04-14 18:54:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,21 @@
 import streamlit as st
 from PIL import Image
+from rembg import remove
+from io import BytesIO
+
 
 
 st.title("Remover de BG")
 
 image_upload = st.file_uploader("Choisir une image",type=['jpg','jpeg','png'])
 
+def convert_image(image):
+  buf = BytesIO()
+  image.save'buf, format='PNG')
+  byte_im = buf.getvalue()
+  return byte_im
+
 if image_upload:
   image = Image.open(image_upload)
+  fixed = remove(image)
+  downloadable_image = convert_image(fixed)
